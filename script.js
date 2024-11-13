0b1eb3edd7fd4eed84e5cadeaea81b0e81839f82
Vane2494 <147008595+Vane2494@users.noreply.github.com>
2024-11-12 22:07:58 -0300

    ajax

--- script.js
+++ script.js
@@ -285,3 +285,35 @@ function cargaLetra(event) {
   }
 
 
+  // Función para cargar una página con AJAX
+
+function loadPage(pageUrl) {
+    fetch(pageUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error en la solicitud: ' + response.status);
+            }
+            return response.text();
+        })
+        .then(html => {
+            document.getElementById('contentContainer').innerHTML = html;
+        })
+        .catch(error => {
+            console.error('Hubo un problema al cargar la página:', error);
+        });
+}
+
+
+document.getElementById('discos').addEventListener('click', function() {
+    loadPage('discos.html');
+});
+
+document.getElementById('videos').addEventListener('click', function() {
+    loadPage('videos.html');
+});
+
+document.getElementById('conciertos').addEventListener('click', function() {
+    loadPage('conciertos.html');
+});
+
+
